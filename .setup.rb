4a30146d96263061abba5ebee08a256fddaf9894
Avi Flombaum <avi@flombaum.com>
2022-11-22 12:21:12 -0500

    Update .setup.rb

--- .setup.rb
+++ .setup.rb
@@ -1,3 +1,5 @@
+github_username = ENV["HOSTNAME"].split("-").first
+
 if !File.exists?("/workspace/.revpro-cli-setup")
   system("git clone https://github.com/revature-curriculum/revpro-cli /workspace/revpro-cli")
   system("cd /workspace/revpro-cli && bundle install && rake install")
@@ -5,7 +7,7 @@ if !File.exists?("/workspace/.revpro-cli-setup")
 end
 
 if !File.exists?("/workspace/.pep-labs-started")
-  system("revpro start https://github.com/aviflombaum/pep-labs /workspace/pep-labs")
+  system("revpro start https://github.com/#{github_username}/pep-labs /workspace/pep-labs")
   system("touch /workspace/.pep-labs-started")
   system("cd /workspace/pep-labs && revpro open Intro_To_Java/Start")
 end
