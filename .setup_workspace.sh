56c12db20af52608cec484b9e9110d98d9c3bbdf
akilan-revature <114386140+akilan-revature@users.noreply.github.com>
2022-12-15 00:18:18 +0530

    Update .setup_workspace.sh

--- .setup_workspace.sh
+++ .setup_workspace.sh
@@ -8,9 +8,20 @@ echo "Repo cloned to /workspace/revpro-cli"
 fi
 
 cd /workspace/revpro-cli
-echo "Changed to /workspace/revpro-cli directory"
 
 bundle install
 rake install
 echo "Installed all dependencies."
+
+
+
+if [[ ! -d "/workspace/revpro-cli/pep-labs" ]] ;
+then
+cd /workspace/revpro-cli/pep-labs
+echo "Changed to /workspace/revpro-cli/pep-labs directory"
+else
+cd /workspace/revpro-cli
+echo "Changed to /workspace/revpro-cli directory"
+fi
+
 echo "Your workspace is ready. Happy coding!"
